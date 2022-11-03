f43781b0936d71bb285e757122e584bcdb4b4643
lilian malukeh <Zinehli2013@gmail.com>
2022-11-03 11:22:56 -0400

    adding web server infra

---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "AMI_ID" {
+    type = string
+    description = "AMI ID"
+    default = "ami-017c001a88dd93847"
+  
+}
+
+variable "INSTANCE_TYPE" {
+    type = string
+    description = "type of EC2 instance"
+    default = "t2.micro"
+  
+}
